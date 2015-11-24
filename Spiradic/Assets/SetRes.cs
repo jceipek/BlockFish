147255876806f5baca12bd13c9eab6a3ba549898
Julian Ceipek <julian.ceipek@gmail.com>
2015-11-23 20:17:20 -0800

    New fish

---
+++ Spiradic/Assets/SetRes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRes : MonoBehaviour {
+	void Start () {
+	   Screen.SetResolution (1920, 1080, true);
+	}
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Application.LoadLevel(1);
+        }
+    }
+}
